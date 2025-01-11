2624beb09c782efeaf4b33f713af8e2e0fdcfadc
pratham404 <pratham.bhavsar501@gmail.com>
2025-01-11 12:22:51 +0530

    added icons

---
+++ lib/constants/app_icons.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class AppIcons {
+  AppIcons._();
+
+  static const _kFontFam = 'icomoon';
+  static const String? _kFontPkg = null;
+
+  static const IconData timer =
+      IconData(0xe901, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData user =
+      IconData(0xe900, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData arrow =
+      IconData(0xe902, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
